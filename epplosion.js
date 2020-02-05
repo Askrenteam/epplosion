cbdc4456e7362d67df12d71fe77b2631109af71c
Renaud <renaud.meurisse@afnic.fr>
2020-02-05 18:12:07 +0100

    change var to const

--- epplosion.js
+++ epplosion.js
@@ -1,16 +1,19 @@
-var net = require('net');
-var readline = require('readline');
+#! node
+const net = require('net');
+const readline = require('readline');
 
-var green = "\x1b[32m";
-var purple = "\x1b[35m";
-var blue = "\x1b[36m";
-var nocolor = "\x1b[0m"
+const green = "\x1b[32m";
+const purple = "\x1b[35m";
+const blue = "\x1b[36m";
+const nocolor = "\x1b[0m"
 
-var highlight = code => {
-    return code.replace(/<(\/?\w+)(.*?)>/g, `${green}<${blue}$1${purple}$2${green}>${nocolor}`);
-}
+const highlight = code =>
+    code.replace(
+        /<(\/?\w+)(.*?)>/g,
+        `${green}<${blue}$1${purple}$2${green}>${nocolor}`
+    );
 
-var sock = net.connect({port: 7000, host: "localhost"});
+const sock = net.connect({port: 7000, host: "localhost"});
 
 sock.on("data", d => {
     console.log();
@@ -28,7 +31,7 @@ const getLengthHeader = i => {
         i&(2**8-1)]);
 }
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
     terminal: true
@@ -39,7 +42,7 @@ rl.on('line', line => {
         sock.end;
         process.exit();
     }
-    var buffer = Buffer.from(line.trim());
+    const buffer = Buffer.from(line.trim());
     sock.write(getLengthHeader(buffer.byteLength));
     sock.write(buffer);
 });
