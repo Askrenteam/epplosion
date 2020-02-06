ed88623cb247f68d4025228d963868e47f1a1ba5
Renaud <renaud.meurisse@afnic.fr>
2020-02-06 14:02:40 +0100

    args parsing + help message

--- epplosion.js
+++ epplosion.js
@@ -2,6 +2,39 @@
 const net = require('net');
 const readline = require('readline');
 
+let serverOptions = {
+    host: "localhost",
+    port: 7000
+}
+
+let helpMessage = 
+`Epplosion - EPP Client
+    -u --url\t URL of the EPP server
+    -p --port\t port of the EPP server
+    -h --help\t this help message
+`
+let args = process.argv.slice(2);
+let arg;
+while (arg = args.shift()) {
+    switch (arg) {
+        case "-u":
+        case "--url":
+            serverOptions.host = args.shift();
+            break;
+        case "-p":
+        case "--port":
+            serverOptions.port = parseInt(args.shift());
+            break;
+        case "-h":
+        case "--help":
+            console.log(helpMessage);
+            process.exit();
+        default:
+            console.error(`unrecognised option : ${arg}`);
+            process.exit(1);
+    }
+}
+
 const green = "\x1b[32m";
 const purple = "\x1b[35m";
 const blue = "\x1b[36m";
@@ -13,17 +46,21 @@ const highlight = code =>
         `${green}<${blue}$1${purple}$2${green}>${nocolor}`
     );
 
-const sock = net.connect({port: 7000, host: "localhost"});
-
+const sock = net.connect(serverOptions);
 sock.on("data", d => {
     console.log();
     console.log(highlight(d.slice(4).toLocaleString()));
 });
+sock.on("error", e => {
+    console.error(e);
+    process.exit(1);
+})
 
 const getLengthHeader = i => {
     i += 4;
     if (i > 2**32) {
-        throw new Error("Message too long")
+        console.error("Message too long");
+        process.exit(1);
     }
     return Uint8Array.from([(i>>(3*8))&(2**8-1), 
         (i>>(2*8))&(2**8-1),
